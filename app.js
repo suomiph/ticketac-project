e557b6533288a1b1bbe4956b084ba15733fc5adf
suomiph <suomiponce@protonmail.com>
2020-01-30 17:09:27 +0100

    add routes login

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(session({
 
 // creates routes
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/login');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', usersRouter);
 
 
 // catch 404 and forward to error handler
