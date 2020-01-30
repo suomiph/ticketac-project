0156480800414731889f1e2380eac28ff7611b2b
suomiph <suomiponce@protonmail.com>
2020-01-30 13:21:00 +0100

    login

--- routes/users.js
+++ routes/users.js
@@ -9,10 +9,65 @@ var session = require('express-session');
 
 var userModel = require('../models/users');
 
+/* Routes user's homepage */
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('login', { title: 'Ticketac' });
+});
+
+
+// 
+//	function (res, req, next) {
+//		if ( typeof(req.session.userid) == "undefined" ) {
+//			res.redirect('/');
+//		}	else next();
+//	},
+
+router.post('/sign-up', async function(req, res, next) {
+	
+	var userInfo = await userModel.findOne( { email: req.body.email } );
+
+	if (userInfo) {
+		res.render('login', { error: "Email already registered \n Please, sign-in" });
+		return ;
+	}
+	
+	var userObj = new userModel( {
+		firstname: req.body.username,
+		lastname: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+	} );
+		
+	var userSaved = await userObj.save();
+	
+	req.session.userid = userSaved.id;
+	req.session.username = userSaved.username;
+	res.redirect('/index');
+}); 
+
+
+
+router.post('/sign-in', async function(req, res, next) {
+	
+	var userInfo = await userModel.findOne( { email: req.body.email, password:req.body.password } );
+	
+	if (userInfo) {
+		req.session.userid = userInfo.id;
+		req.session.username = userInfo.username;
+		res.redirect('/index');
+	} else {
+		res.render('login', { error: "Invalid email or password" });
+	}
+
+});
+
+router.get('/logout', async function(req, res, next) {
+	
+	req.session.destroy( function () {
+		res.render('login', { title: 'Ticketac', error: "See you soon !" })
+	});
+	
 });
 
 
